8fa7064f0590ca3152fea2091c77829f6c90c5c7
LEEHYEONG DO <hyeongdo@LEEHYEONGui-MacBook-Pro.local>
2017-04-29 18:03:33 +0900

    Add - comment reply and modify

--- blog/models.py
+++ blog/models.py
@@ -1,13 +1,41 @@
+import re
+from django import forms
 from django.db import models
+from django.conf import settings
 
+
+def lnglat_validator(value):
+    if not re.match(r'^[+-]?\d+\.?\d*, [+-]?\d+\.?\d*', value):
+        raise forms.ValidationError('경도위도를 입력해 주세요')
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     photo = models.ImageField()
+    lnglat = models.CharField(max_length=40, blank=True, validators=[lnglat_validator])
     is_public = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
+
+    @property
+    def lng(self):
+        if self.lnglat:
+            return self.lnglat.split(',')[0]
+        return None
+
+    @property
+    def lat(self):
+        if self.lnglat:
+            return self.lnglat.split(',')[1]
+        return None
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 from .models import Post
+from .forms import CommentForm
 
 
 def index(request):
@@ -13,4 +15,22 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {
         'post': post
+    })
+
+
+@login_required
+def comment_new(request, post_pk):
+    post = get_object_or_404(Post, pk=post_pk)
+    if request.method == 'POST':
+        form = CommentForm(request.POST, request.FILES)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.author = request.user
+            comment.save()
+            return redirect('blog:post_detail', post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/comment_form.html', {
+        'form': form,
     })
